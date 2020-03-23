fa0d28fe3b5acf7cea165523beb19d8f8f7e45b3
guohongtong <guohongtong@zhihu.com>
2020-03-23 15:03:50 +0800

    ci

--- script/sync.sh
+++ script/sync.sh
@@ -1,2 +1,3 @@
 tar -czvf source.tar.gz ./build
 scp ./source.tar.gz $USERNAME@$SERVER:/var/www/html/source.tar.gz
+ssh $USERNAME@$SERVER -o StrictHostKeyChecking=no 'cd /var/www/ && rm -rf ./react && tar -xzvf source.tar.gz && mv build react && rm source.tar.gz'
